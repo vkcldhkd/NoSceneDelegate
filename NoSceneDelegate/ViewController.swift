b55b604f82a4c0bc3f862b3a5a36d10e3921ad27
Sung <vkcldhkd@gmail.com>
2020-12-23 10:26:45 +0900

    Initial Commit

---
+++ NoSceneDelegate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NoSceneDelegate
+//
+//  Created by SUNG on 2020/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
